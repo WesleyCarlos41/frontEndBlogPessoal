e69a85559983688a668ed3ed652bba0cc33b5e47
WesleyCarlos41 <102267247+WesleyCarlos41@users.noreply.github.com>
2022-05-28 23:15:28 -0300

    Configurando entar e cadastrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
+import { EntarComponent } from './entar/entar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    RodapeComponent
+    RodapeComponent,
+    EntarComponent,
+    CadastrarComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/entar/entar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entar',
+  templateUrl: './entar.component.html',
+  styleUrls: ['./entar.component.css']
+})
+export class EntarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
