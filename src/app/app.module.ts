bb64b58200c46199ce30f7865c71291d6d69d949
WesleyCarlos41 <102267247+WesleyCarlos41@users.noreply.github.com>
2022-06-05 16:16:54 -0300

    Estilizando o componente tema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { RodapeComponent } from './rodape/rodape.component';
 import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { TemaComponent } from './tema/tema.component';
 
 
 
@@ -23,6 +24,7 @@ import { InicioComponent } from './inicio/inicio.component';
     EntrarComponent,
     CadastrarComponent,
     InicioComponent,
+    TemaComponent,
   ],
   imports: [
     BrowserModule,
